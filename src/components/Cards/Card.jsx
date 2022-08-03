9495eba37af57344d0dc46fc9215d9cff424f45a
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2022-08-03 11:15:56 +0530

    Tilt effect

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import '../Cards/Cards.css';
+import Tilt from 'react-vanilla-tilt';
 
-
-function Card({heading,disc}) {
+function Card({ heading, disc }) {
   return (
     <>
-        <div className="card">
+      {/* <div className="card">
             <h2>{heading}</h2>
             <div className="disc">{disc}</div>
             <button className='btn' >Register</button>
-        </div>
-        {/* <div className="card">
-            <h2>Heading</h2>
-            <div className="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci tenetur dignissimos minus error laboriosam. Facere amet nemo laboriosam aliquid soluta, expedita similique tempora .</div>
-            <button className='btn' >Register</button>
         </div> */}
-
+      {/* <div className="card">
+        <h2>Heading</h2>
+        <div className="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci tenetur dignissimos minus error laboriosam. Facere amet nemo laboriosam aliquid soluta, expedita similique tempora .</div>
+        <button className='btn' >Register</button>
+      </div> */}
+      <Tilt options={{scale: 2, max: 25}}  style={{ height: 300, width: 300 }}>
+      <div className="card">
+        <h2>Heading</h2>
+        <div className="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci tenetur dignissimos minus error laboriosam. Facere amet nemo laboriosam aliquid soluta, expedita similique tempora .</div>
+        <button className='btn' >Register</button>
+      </div>
+      </Tilt>
     </>
   )
 }
