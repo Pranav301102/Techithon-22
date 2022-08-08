2768bf0450968b72e51d24b24f2cb816a21dcae6
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2022-08-08 16:54:35 +0530

    About Us page ka ek card dala hai

--- src/components/Pages/About/about.jsx
+++ src/components/Pages/About/about.jsx
@@ -1,14 +1,14 @@
 import "./about.css";
-import React, { useState, useRef, useEffect,  } from "react";
+import React, { useState, useRef, useEffect, } from "react";
 import NET from "vanta/dist/vanta.net.min";
 
 export default function About() {
   const [vantaEffect, setVantaEffect] = useState(0);
   const myRef = useRef(null);
   useEffect(() => {
-      if (!vantaEffect) {
-        setVantaEffect(
-          NET({
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
           el: myRef.current,
           color: 0x14b679,
           backgroundColor: 0x15173c,
@@ -19,21 +19,48 @@ export default function About() {
           points: 20.00,
           maxDistance: 30.00,
           spacing: 25.00
-          })
-        );
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) {
+        vantaEffect.destroy()
+        console.log("instance distroyed")
       }
-      return () => {
-        if (vantaEffect) 
-        {vantaEffect.destroy()
-          console.log("instance distroyed")
-        }
-      };
-    }, [vantaEffect]);
-    return(
-      <div className="background-page" ref={myRef}>
+    };
+  }, [vantaEffect]);
+  return (
+    //   <div className="background-page" ref={myRef}>
 
-    </div>
-    );
+    // </div>
+    <>
+      <section>
+        <div class="container">
+          <div class="card">
+            <div class="content">
+              <div class="imgBx">
+                <img src="https://image.flaticon.com/icons/png/256/4213/4213641.png" />
+              </div>
+              <div class="contentBx">
+                <h3>Giraffe</h3>
+              </div>
+            </div>
+            <ul class="sci">
+              <li>
+                <a href=""><button>Icon</button></a>
+              </li>
+              <li>
+                <a href=""><button>Icon</button></a>
+              </li>
+              <li>
+                <a href=""><button>Icon</button></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 
