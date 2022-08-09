75b76dab66169dbf652a46b545155df28cbdecf4
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2022-08-09 16:53:03 +0530

    Events

---
+++ src/components/Pages/Events/EventData.jsx
@@ -0,0 +1,259 @@
+export const EventsData = [
+    {
+        "id": 1,
+        "name" : "Tech Events",
+        // "image" : require("../images/trippy_element_1.png"),
+        "events" : [
+            {
+                // "image" : require("../images/webp/web.webp"),
+                "name" : "Web Programming Pillars (HTML & CSS)",
+                "description" : "You are the CSS to my HTML",
+                "price" : "200 PP"
+            },
+            {
+                // "image" : require("../images/webp/make_a_bot.webp"),
+                "name" : "Make a bot",
+                "description" : "Automate things using a bot",
+                "price" : "200 PP"
+            },
+            {
+                // "image" : require("../images/webp/uncharted.webp"),
+                "name" : "Uncharted (Treasure Hunt)",
+                "description" : "Track, Trail, & Tail",
+                "price" : "200 PP"
+            },
+            {
+                // "image" : require("../images/webp/rocket.webp"),
+                "name" : "Bottle Rocket",
+                "description" : "Sending it to Mars maybe?",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/line_follower.webp"),
+                "name" : "Line Follower Robot",
+                "description" : "No wrong turns",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/fast_and_furious.webp"),
+                "name" : "Fast and furious",
+                "description" : "Start your engines",
+                "price" : "200 PP",
+                "type" : [
+                    "Wired",
+                    "Not Wired"
+                ]
+            }
+        ]
+    },
+    // {
+    //     "id": "2",
+    //     "name" : "Hackathon (Hack For Change)",
+    //     "image" : require("../images/trippy_element_1.png"),
+    //     "events" : [
+    //         {
+    //             "image" : require("../images/web.png"), 
+    //             "name" : "Hackathon",
+    //             "description" : "Can't find a solution? Let's build one",
+    //             "price" : "0"
+    //         }
+    //     ]
+    // },
+    { 
+        "id": 2,
+        "name" : "Workshops",
+        "image" : require("../images/trippy_element_1.png"),
+        "events" : [
+            {
+                "image" : require("../images/webp/web_3.webp"),
+                "name" : "Web 3.0",
+                "description" : "An Intro to the new and decentralised web",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/Crypto_Finance.webp"),
+                "name" : "Crypto and Finance",
+                "description" : "The new level of currency",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/design.webp"),
+                "name" : "Design Simplified",
+                "description" : "Where Design Comes to Life",
+                "description2" : "Adding Creativity to your Triumph",
+                "description3" : "Creativity meets technology",
+                "price" : "200 PP"
+
+            },
+            {
+                "image" : require("../images/webp/discord.webp"),
+                "name" : "Discord Bots 101",
+                "description" : "Bring your imagination to discord :D",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/flutter.webp"),
+                "name" : "Flutter Workshop",
+                "description" : "Everything is a widget",
+                "price" : "200 PP"
+            },
+            {
+                "image" : require("../images/webp/nvidia.webp"),
+                "name" : "Nvidia Workshop",
+                "description" : "Deep learning for computer vision",
+                "price" : "200 PP"
+            }
+        ]
+    },
+    // {
+    //     "id": "4",
+    //     "name" : "Gaming Events",
+    //     "image" : require("../images/trippy_element_1.png"),
+    //     "events" : [
+    //         {
+    //             "image" : require("../images/csgo.png"), 
+    //             "name" : "CS-GO",
+    //             "description" : "Are we just rushin' in? Or are we goin' all sneaky beaky like? Let's bring it on!",
+    //             "price" : "100",
+    //             "group" : 5
+    //         },
+    //         {
+    //             "image" : require("../images/valorant.png"), 
+    //             "name" : "Valorant",
+    //             "description" : "Sheeeshh! Neon's gonna get' em all!... OMG LAG!!",
+    //             "price" : "100",
+    //             "group" : 5
+    //         },
+    //         {
+    //             "image" : require("../images/pubg.png"),     
+    //             "name" : "Pubg Mobile",
+    //             "description" : "Winner Winner chicken dinner",
+    //             "price" : "100",
+    //             "group" : 4
+    //         }
+    //     ]
+    // },
+    {
+        "id": 3,
+        "name" : "Cultural Events",
+        "image" : require("../images/trippy_element_1.png"),
+        "events" : [
+            {
+                "image" : require("../images/webp/dance.webp"),
+                "name" : "Aaja Nachle",
+                "description" : "It's the time to disco",
+                "price" : "100 PP",
+                "group" : true
+            },
+            {
+                "image" : require("../images/webp/music.webp"),
+                "name" : "Yuva Studio",
+                "description" : "Bless us with your lovely tone.",
+                "price" : "100 PP",
+                "group" : true
+            },
+            {
+                "image" : require("../images/webp/fashion_show.webp"),
+                "name" : "Yuva's Ramp show",
+                "description" : "Fashion ka hai yeh jalwa",
+                "price" : "100 PP",
+                "group" : false
+            },
+            {
+                "image" : require("../images/webp/drama.webp"),
+                "name" : "Oscar Academy",
+                "description" : "Don't explain, it's time to express.",
+                "price" : "100 PP",
+                "group" : true
+            },
+            {
+                "image" : require("../images/webp/band_show.webp"),
+                "name" : "Rock with Band",
+                "description" : "So who are the next backstreet boys?",
+                "price" : "100 PP",
+                "group" : true
+            },
+            {
+                "image" : require("../images/webp/rangoli.webp"),
+                "name" : "Rango ki mehfil",
+                "description" : "kuch Rang apne hunar ke.",
+                "price" : "50 PP",
+                "group" : true
+            },
+            {
+                "image" : require("../images/webp/painting.webp"),
+                "name" : "Painter's Den",
+                "description" : "Let the canvas do the talking",
+                "price" : "50 PP",
+                "group" : false
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "name" : "Sports Events",
+        "image" : require("../images/trippy_element_1.png"),
+        "events" : [
+            {
+                "image" : require("../images/webp/cricket.webp"),
+                "name" : "Box Cric",
+                "description" : "Either a six or wicket, but the watch shows time for Super-Over. (7-a-side)",
+                "price" : "1600",
+                "group" : "Group Event",
+                "size" : "Maximum people: 8"
+            },
+            {
+                "image" : require("../images/webp/football.webp"),
+                "name" : "Yuva league",
+                "description" : "Some dream about goals-we make them (6-a-side)",
+                "price" : "1400",
+                "group" : "Group Event",
+                "size" : "Maximum people: 7"
+            },
+            {
+                "image" : require("../images/webp/basketball.webp"),
+                "name" : "Dribble",
+                "description" : "let's make a hoop for Kobe (5-a-side)",
+                "price" : "1200",
+                "group" : "Group Event",
+                "size" : "Maximum people: 6"
+            },
+            {
+                "image" : require("../images/webp/chess.webp"),
+                "name" : "Game of Thrones",
+                "description" : "Don't let the king die",
+                "price" : "50",
+                "group" : false
+            },
+            {
+                "image" : require("../images/webp/carrom.webp"),
+                "name" : "Flick",
+                "description" : "Rani toh Pappa nij",
+                "price" : "70",
+                "group" : false
+            },
+            {
+                "image" : require("../images/webp/table_tennis.webp"),
+                "name" : "Ping Pong (Table Tennis)",
+                "description" : "Eat. Sleep. Ping Pong.",
+                "price" : "100",
+                "group" : false
+            },
+            {
+                "image" : require("../images/webp/tug_of_war.webp"),
+                "name" : "Rope dopes",
+                "description" : "A war fought with ropes ",
+                "price" : "400",
+                "group" : "Group Event",
+                "size" : "Group Size: 8"
+            },
+            {
+                "image" : require("../images/webp/frisbee.webp"),
+                "name" : "Discy Wing",
+                "description" : "It's time to show your frisbee skills",
+                "price" : "60",
+                "group" : false
+            }
+        ]
+    }
+];
