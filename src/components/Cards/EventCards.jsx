4c4400b456c70334f69aa185b934a9b98448ddac
Pranav <pranavtrivedi02@gmail.com>
2022-08-12 00:51:21 +0530

    Added  carousel

--- src/components/Cards/EventCards.jsx
+++ src/components/Cards/EventCards.jsx
@@ -4,12 +4,21 @@ import Card from "./Card";
 import { EventsData } from "../Pages/Events/EventData";
 import { CreateCard } from "../Pages/Events/Events";
 import { CardGrid } from "../Pages/Events/Events";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useEffect } from "react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+import { styled } from "styled-components";
 
 const PriorityEvents = EventsData.filter(
   (EventsData) => EventsData.priority === true
 );
 
-function EventCards() {
+export default function EventCards() {
   return (
     <>
       <CardGrid>{PriorityEvents.map(CreateCard)}</CardGrid>
@@ -17,4 +26,53 @@ function EventCards() {
   );
 }
 
-export default EventCards;
+function NumCard() {
+  const [windowDimensions, setWindowDimensions] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  const num = windowDimensions > 500 ? 4 : 1;
+  return num;
+}
+
+export function Eventcarousel() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={NumCard()}
+        spaceBetween={30}
+        slidesPerGroup={NumCard()}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {PriorityEvents.map(CarouselCard)}
+      </Swiper>
+    </>
+  );
+}
+
+function CarouselCard(Events) {
+  return (
+    <SwiperSlide>
+      <Card
+        name={Events.name}
+        disc={Events.disc}
+        key={Events.id}
+        time={Events.time}
+        date={Events.date}
+        venue={Events.venue}
+        reg={Events.reg}
+      />
+    </SwiperSlide>
+  );
+}
