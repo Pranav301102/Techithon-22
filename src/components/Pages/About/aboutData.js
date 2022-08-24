a5173f32e9e588553aa65967fbbfc02bcb013ebd
Your Name <you@example.com>
2022-08-24 13:53:37 +0530

    added photos

--- src/components/Pages/About/aboutData.js
+++ src/components/Pages/About/aboutData.js
@@ -1,130 +1,246 @@
 // catogory head asst top
 
-export const AboutData = [
-    {
-        id:1,
-        category:"top",
-        post:"Shinigami",
+export const AboutData = [{
+        id: 1,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Aastha edited.jpg"
     },
     {
-        id:2,
-        category:"top",
-        post:"Shinigami",
+        id: 2,
+        category: "head",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Adarsh Dubey - Technical head -min.JPG"
     },
     {
-        id:3,
-        category:"top",
-        post:"Shinigami",
+        id: 3,
+        category: "top",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Anuj Shinde_Vice Chairperson 1-min.jpg"
     },
     {
-        id:4,
-        category:"head",
-        post:"Shinigami",
+        id: 4,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Arnav edited-min.jpg"
     },
     {
-        id:5,
-        category:"head",
-        post:"Shinigami",
+        id: 5,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Aryan Jadhav_ Assistant Registration Head-min.jpg"
     },
     {
-        id:6,
-        category:"head",
-        post:"Shinigami",
+        id: 6,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Badal edited-min.jpg"
     },
     {
-        id:7,
-        category:"head",
-        post:"Shinigami",
+        id: 7,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Chaitanaya  edited-min.jpg"
     },
     {
-        id:8,
-        category:"asst",
-        post:"Shinigami",
+        id: 8,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Chinmay edited-min.jpg"
     },
     {
-        id:9,
-        category:"asst",
-        post:"Shinigami",
+        id: 9,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Chinmayee Bidaye_Asst. Digital Creative Head-min.jpg"
     },
     {
-        id:10,
-        category:"asst",
-        post:"Shinigami",
+        id: 10,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Deepansh Londhe_ Assistant Registration Head-min.jpg"
     },
     {
-        id:11,
-        category:"asst",
-        post:"Shinigami",
+        id: 11,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Harsheet jani edited-min.jpg"
     },
     {
-        id:12,
-        category:"asst",
-        post:"Shinigami",
+        id: 12,
+        category: "head",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Kaushik Gudalkar_Registration Head-min.JPG"
     },
     {
-        id:13,
-        category:"asst",
-        post:"Shinigami",
+        id: 13,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
+        instagram: "",
+        linkdin: "",
+        img: "img/Kedar Khamkar _Assitant Creative Head -min.jpg"
     },
     {
-        id:14,
-        category:"asst",
-        post:"Shinigami",
+        id: 14,
+        category: "asst",
+        post: "Shinigami",
         name: "Zoro uchiha",
-        instagram:"",
-        linkdin:"",
-        img:"zoro.jpg"
-    },   
+        instagram: "",
+        linkdin: "",
+        img: "img/Nimishaa edited-min.jpg"
+    },
+    {
+        id: 15,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Omkar ayare_ event coordinator -min.jpg"
+    },
+    {
+        id: 16,
+        category: "asst",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/pooja patil-min.jpg"
+    },
+    {
+        id: 17,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Prasad-min.jpg"
+    },
+    {
+        id: 18,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Sanjana Pawar Photo Formals-min-min.JPG"
+    },
+    {
+        id: 19,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Sanya Shaikh_Creative Head-min.jpg"
+    },
+    {
+        id: 20,
+        category: "top",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Soham Shahane_CEC-min.jpeg"
+    },
+    {
+        id: 21,
+        category: "asst",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Tanaya Lad_Asst. Creative Head-min.jpg"
+    },
+    {
+        id: 22,
+        category: "asst",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/UAditiya edited-min.jpg"
+    },
+    {
+        id: 23,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Vans vu.jpg"
+    },
+    {
+        id: 23,
+        category: "top",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Anurag Joshi.jpg"
+    },
+    {
+        id: 23,
+        category: "head",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Abdul.jpg"
+    },
+    {
+        id: 23,
+        category: "asst",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "img/Agneya .jpg"
+    },
+    {
+        id: 23,
+        category: "asst",
+        post: "Shinigami",
+        name: "Zoro uchiha",
+        instagram: "",
+        linkdin: "",
+        img: "zoro.jpg"
+    },
 ]
