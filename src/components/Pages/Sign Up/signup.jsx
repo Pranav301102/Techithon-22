0d0adc0d2908b296c5aa5d059d7aa8780cc1dc59
Aryan Chavan <aryan@aryansdomain.com>
2022-08-28 01:12:33 +0530

    remove social login and add name field

--- src/components/Pages/Sign Up/signup.jsx
+++ src/components/Pages/Sign Up/signup.jsx
@@ -5,29 +5,29 @@ import Button from "./ButtonSignUp";
 import Icon from "./IconSignUp";
 import Input from "./InputSignUp";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import { CustomScrollbars } from './../Events/SideBar';
+import { CustomScrollbars } from "./../Events/SideBar";
 
 function signup() {
-  const FacebookBackground =
-    "linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
-  const InstagramBackground =
-    "linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
-  const TwitterBackground =
-    "linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
-  return (
-    <Backgrond>
-      <MainContainer>
-        <WelcomeText>Welcome</WelcomeText>
-        <InputContainer>
-          <Input type="text" placeholder="Email" />
-          <Input type="password" placeholder="Password" />
-          <Input type="password" placeholder="Password" />
-          <Input type="password" placeholder="Password" />
-        </InputContainer>
-        <ButtonContainer>
-          <Button content="Sign Up" />
-        </ButtonContainer>
-        <LoginWith>OR Sign Up WITH</LoginWith>
+	const FacebookBackground =
+		"linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
+	const InstagramBackground =
+		"linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
+	const TwitterBackground =
+		"linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
+	return (
+		<Backgrond>
+			<MainContainer>
+				<WelcomeText>Welcome</WelcomeText>
+				<InputContainer>
+					<Input type="text" placeholder="Name" />
+					<Input type="email" placeholder="Email" />
+					<Input type="password" placeholder="Password" />
+					<Input type="password" placeholder="Password" />
+				</InputContainer>
+				<ButtonContainer>
+					<Button content="Sign Up" />
+				</ButtonContainer>
+				{/* <LoginWith>OR Sign Up WITH</LoginWith>
         <HorizontalRule />
         <IconsContainer>
           <Icon color={FacebookBackground}>
@@ -39,128 +39,123 @@ function signup() {
           <Icon color={TwitterBackground}>
             <FaTwitter />
           </Icon>
-        </IconsContainer>
-       {/* <ForgotPassword>Forgot Password ?</ForgotPassword> */}
-      </MainContainer>
-    </Backgrond>
-  );
+        </IconsContainer> */}
+				{/* <ForgotPassword>Forgot Password ?</ForgotPassword> */}
+			</MainContainer>
+		</Backgrond>
+	);
 }
 
-
-
 const MainContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  height: 80vh;
-  width: 30vw;
-  position: absolute;   
-  z-index: 50;
-  background: rgba(34,40,49, 0.45);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(8.5px);
-  -webkit-backdrop-filter: blur(8.5px);
-  border-radius: 10px;
-  color: #ffffff;
-  text-transform: uppercase;
-  letter-spacing: 0.4rem;
-  overflow-y: auto  ;
-  @media only screen and (max-width: 320px) {
-    width: 80vw;
-    height: 80vh;
-    top: 10%;
-    left: 2%;
-    hr {
-      margin-bottom: 0.3rem;
-    }
-    h4 {
-      font-size: small;
-    }
-  }
-  @media only screen and (min-width: 360px) {
-    width: 80vw;
-    height: 80vh;
-    top: 10%;
-    
-    h4 {
-      font-size: small;
-    }
-  }
-  @media only screen and (min-width: 411px) {
-    width: 80vw;
-    height: 80vh;
-    top: 10%;
-    left: 10%
-  }
-  @media only screen and (min-width: 768px) {
-    width: 80vw;
-    height: 80vh;
-    top: 10%;
-    left: 10%
-  }
-  @media only screen and (min-width: 1024px) {
-    width: 70vw;
-    height: 80vh;
-    top: 10%;
-    left: 10%
-  }
-  @media only screen and (min-width: 1280px) {
-    width: 30vw;
-    height: 80vh;
-    top: 10%;
-    left: 10%
-  }
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	height: 80vh;
+	width: 30vw;
+	position: absolute;
+	z-index: 50;
+	background: rgba(34, 40, 49, 0.45);
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+	backdrop-filter: blur(8.5px);
+	-webkit-backdrop-filter: blur(8.5px);
+	border-radius: 10px;
+	color: #ffffff;
+	text-transform: uppercase;
+	letter-spacing: 0.4rem;
+	overflow-y: auto;
+	@media only screen and (max-width: 320px) {
+		width: 80vw;
+		height: 80vh;
+		top: 10%;
+		left: 2%;
+		hr {
+			margin-bottom: 0.3rem;
+		}
+		h4 {
+			font-size: small;
+		}
+	}
+	@media only screen and (min-width: 360px) {
+		width: 80vw;
+		height: 80vh;
+		top: 10%;
+
+		h4 {
+			font-size: small;
+		}
+	}
+	@media only screen and (min-width: 411px) {
+		width: 80vw;
+		height: 80vh;
+		top: 10%;
+		left: 10%;
+	}
+	@media only screen and (min-width: 768px) {
+		width: 80vw;
+		height: 80vh;
+		top: 10%;
+		left: 10%;
+	}
+	@media only screen and (min-width: 1024px) {
+		width: 70vw;
+		height: 80vh;
+		top: 10%;
+		left: 10%;
+	}
+	@media only screen and (min-width: 1280px) {
+		width: 30vw;
+		height: 80vh;
+		top: 10%;
+		left: 10%;
+	}
 `;
 
 const WelcomeText = styled.h2`
-  margin: 3rem 0 2rem 0;
+	margin: 3rem 0 2rem 0;
 `;
 
 const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  height: 40vh;
-  width: 100%;
-
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	height: 40vh;
+	width: 100%;
 `;
 
 const ButtonContainer = styled.div`
-  margin: 1rem 0 2rem 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	margin: 1rem 0 2rem 0;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 const LoginWith = styled.h5`
-  cursor: pointer;
+	cursor: pointer;
 `;
 
 const HorizontalRule = styled.hr`
-  width: 90%;
-  height: 0.3rem;
-  border-radius: 0.8rem;
-  border: none;
-  background: linear-gradient(to right, #1F3C67 10%, #00ADB5 79%);
-  background-color: #ebd0d0;
-  margin: 1.5rem 0 1rem 0;
-  backdrop-filter: blur(25px);
-  padding:2px;
+	width: 90%;
+	height: 0.3rem;
+	border-radius: 0.8rem;
+	border: none;
+	background: linear-gradient(to right, #1f3c67 10%, #00adb5 79%);
+	background-color: #ebd0d0;
+	margin: 1.5rem 0 1rem 0;
+	backdrop-filter: blur(25px);
+	padding: 2px;
 `;
 
 const IconsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin: 2rem 0 3rem 0;
-  width: 80%;
+	display: flex;
+	justify-content: space-evenly;
+	margin: 2rem 0 3rem 0;
+	width: 80%;
 `;
 
 const ForgotPassword = styled.h4`
-  cursor: pointer;
+	cursor: pointer;
 `;
-
-
 
 export default signup;
