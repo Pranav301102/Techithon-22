5c8f155cfa0d0c7a5e5fb29a4856d7509308d3eb
Aryan Chavan <aryan@aryansdomain.com>
2022-08-28 23:10:27 +0530

    load events from backend

--- src/components/Pages/Events/Events.jsx
+++ src/components/Pages/Events/Events.jsx
@@ -1,141 +1,150 @@
+import React, { useState, useEffect } from "react";
 import Card from "../../Cards/Card";
 import { EventsData } from "./EventData";
 import styled from "styled-components";
-
+import axios from "axios";
+import config from "../../../config";
 
 export const SpecialAttractionEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "SPECIAL ATTRACTION EVENTS"
+	(EventsData) => EventsData.category === "SPECIAL ATTRACTION EVENTS"
 );
 export const PreEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "PRE EVENTS"
+	(EventsData) => EventsData.category === "PRE EVENTS"
 );
 export const TechEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "TECHNICAL EVENTS"
+	(EventsData) => EventsData.category === "TECHNICAL EVENTS"
 );
 export const FunEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "FUN EVENTS"
+	(EventsData) => EventsData.category === "FUN EVENTS"
 );
 export const GamingEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "GAMING EVENTS"
+	(EventsData) => EventsData.category === "GAMING EVENTS"
 );
 export const NeonEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "NEON EVENTS"
+	(EventsData) => EventsData.category === "NEON EVENTS"
 );
 export const StrongmanEvents = EventsData.filter(
-  (EventsData) => EventsData.category === "STRONGMAN EVENTS"
+	(EventsData) => EventsData.category === "STRONGMAN EVENTS"
 );
 
 export function CreateCard(Events) {
-  return (
-    <>
-      <Card
-        id={Events.id}
-        name={Events.name}
-        disc={Events.disc}
-        key={Events.id}
-        time={Events.time}
-        date={Events.date}
-        venue={Events.venue}
-        reg={Events.reg}
-      />
-    </>
-  );
+	return (
+		<>
+			<Card
+				id={Events._id}
+				name={Events.name}
+				disc={Events.description}
+				key={Events._id}
+				time={Events.time}
+				date={Events.date}
+				venue={Events.venue}
+				reg={`₹${Events.price} ${Events.pricing}`}
+			/>
+		</>
+	);
 }
 
 export const CardGrid = styled.div`
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-  column-gap: 60px;
-  row-gap: 140px;
-  margin-left: 100px;
-  margin-top: 10px;
-  z-index: 50;
-  /* height: 50vh; */
+	display: grid;
+	grid-template-columns: auto auto auto auto;
+	column-gap: 60px;
+	row-gap: 140px;
+	margin-left: 100px;
+	margin-top: 10px;
+	z-index: 50;
+	/* height: 50vh; */
 
-  @media only screen and (max-width: 500px) {
-    display: grid;
-    grid-template-columns: auto;
-    column-gap: 10px;
-    row-gap: 140px;
-    /* width: 400px; */
-    margin-left: 60px;
-    margin-top: 10px;
-    z-index: 50;
-  }
-  /* ipad mini*/
-  @media only screen and (min-width: 768px) and (max-width: 1024px) {
-    display: grid;
-    grid-template-columns: auto auto;
-    column-gap: 50px;
-    row-gap: 140px;
-    width: 400px;
-    margin-left: 70px;
-    margin-top: 10px;
-    z-index: 50;
-  }
-  @media only screen and (min-width: 1024px) and (max-width: 1424px) {
-    display: grid;
-    grid-template-columns: auto auto auto;
-    column-gap: 50px;
-    row-gap: 140px;
-    width: 400px;
-    margin-left: 70px;
-    margin-top: 10px;
-    z-index: 50;
-  }
+	@media only screen and (max-width: 500px) {
+		display: grid;
+		grid-template-columns: auto;
+		column-gap: 10px;
+		row-gap: 140px;
+		/* width: 400px; */
+		margin-left: 60px;
+		margin-top: 10px;
+		z-index: 50;
+	}
+	/* ipad mini*/
+	@media only screen and (min-width: 768px) and (max-width: 1024px) {
+		display: grid;
+		grid-template-columns: auto auto;
+		column-gap: 50px;
+		row-gap: 140px;
+		width: 400px;
+		margin-left: 70px;
+		margin-top: 10px;
+		z-index: 50;
+	}
+	@media only screen and (min-width: 1024px) and (max-width: 1424px) {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		column-gap: 50px;
+		row-gap: 140px;
+		width: 400px;
+		margin-left: 70px;
+		margin-top: 10px;
+		z-index: 50;
+	}
 `;
 
 export function SpecialAttractionCards() {
-  return (
-    <>
-      <CardGrid>{SpecialAttractionEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	const [specialEvents, setSpecialEvents] = useState([]);
+	useEffect(() => {
+		axios.get(`${config.backendLocation}/event`).then((res) => {
+			setSpecialEvents(res.data);
+		});
+	}, []);
+
+	return (
+		<>
+			<CardGrid>{specialEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 
 export function PreCards() {
-  return (
-    <>
-      <CardGrid>{PreEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{PreEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 
 export function TechCards() {
-  return (
-    <>
-      <CardGrid>{TechEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{TechEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 
 export function FunCards() {
-  return (
-    <>
-      <CardGrid>{FunEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{FunEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 
 export function GamingCards() {
-  return (
-    <>
-      <CardGrid>{GamingEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{GamingEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 export function NeonCards() {
-  return (
-    <>
-      <CardGrid>{NeonEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{NeonEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
 
 export function StrongmanCards() {
-  return (
-    <>
-      <CardGrid>{StrongmanEvents.map(CreateCard)}</CardGrid>
-    </>
-  );
+	return (
+		<>
+			<CardGrid>{StrongmanEvents.map(CreateCard)}</CardGrid>
+		</>
+	);
 }
