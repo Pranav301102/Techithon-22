ad3fbd713ed75230eff47d6778c8826fc8bee452
Aryan Chavan <aryan@aryansdomain.com>
2022-08-29 10:48:15 +0530

    add ticket

--- src/components/Ticket/index.jsx
+++ src/components/Ticket/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import axios from "axios";
+import config from "../../config";
 
 function Ticket() {
-  return (
-    <div>index</div>
-  )
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		axios
+			.get(`${config.backendLocation}/user/self`, {
+				headers: { token: localStorage.token },
+			})
+			.then((res) => {
+				setUser(res.data);
+				setInterval(() => {
+					window.print();
+					window.location = "/";
+				}, 3000);
+			});
+	}, []);
+
+	return (
+		<>
+			{user && (
+				<div className="ticket" id="ticket">
+					Name: {user.username}
+					<br />
+					Email: {user.email}
+					<br />
+					<img
+						className="qr"
+						src={`https://api.qrserver.com/v1/create-qr-code/?size=600x600&data=${user._id}`}
+					/>
+				</div>
+			)}
+		</>
+	);
 }
 
-export default Ticket
+export default Ticket;
