a17c540185491c8f5652e223c04c92e216965015
Sahil8383 <95122593+Sahil8383@users.noreply.github.com>
2022-09-05 10:58:12 +0530

    Button added in pre-events card

--- src/components/Pages/Home-Page/Highlights.jsx
+++ src/components/Pages/Home-Page/Highlights.jsx
@@ -1,33 +1,36 @@
 import './Highlights.css'
 
-export function Highlights(){
-    return(
-    <div class="content demo">
-        <div class="card-hghlght demo-card">
-            <h1>DAY 1</h1>
+export function Highlights() {
+    return (
+        <div class="content demo">
+            <div class="card-hghlght demo-card">
+                <h1>DAY 1</h1>
+            </div>
+            <div class="card-hghlght demo-card">
+                <h1>DAY 2</h1>
+            </div>
+            <div class="card-hghlght demo-card">
+                <h1>DAY 3</h1>
+            </div>
         </div>
-        <div class="card-hghlght demo-card">
-            <h1>DAY 2</h1>
-        </div>
-        <div class="card-hghlght demo-card">
-            <h1>DAY 3</h1>
-        </div>     
-      </div>
     )
 }
 
-export function PreEvents(){
-    return(
-    <div class="content demo">
-        <div class="card-hghlght demo-card">
-            <img src="/img/3.jpg " alt="3"></img>
+export function PreEvents() {
+    return (
+        <div class="content demo">
+            <div class="card-hghlght demo-card">
+                <img src="/img/3.jpg " alt="3"></img>
+                <button className='btn' >Register</button>
+            </div>
+            <div class="card-hghlght demo-card">
+                <img src="/img/8.jpg " alt="8"></img>
+                <button className='btn' >Register</button>
+            </div>
+            <div class="card-hghlght demo-card">
+                <img src="/img/7.jpg " alt="7"></img>
+                <button className='btn' >Register</button>
+            </div>
         </div>
-        <div class="card-hghlght demo-card">
-        <img src="/img/8.jpg " alt="8"></img>        
-        </div>
-        <div class="card-hghlght demo-card">
-        <img src="/img/7.jpg " alt="7"></img>        
-        </div>     
-      </div>
     )
 }
