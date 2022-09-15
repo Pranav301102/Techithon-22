a3118fd10f3418eea4e1882e991a57ab9fc254e6
Pranav <pranavtrivedi02@gmail.com>
2022-09-15 22:45:14 +0530

    Minor changes

--- src/components/Pages/Home-Page/Highlights.jsx
+++ src/components/Pages/Home-Page/Highlights.jsx
@@ -1,4 +1,6 @@
 import './Highlights.css'
+import { useNavigate } from "react-router-dom";
+
 
 export function Highlights() {
     return (
@@ -17,19 +19,24 @@ export function Highlights() {
 }
 
 export function PreEvents() {
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+    let path = `/events`; 
+    navigate(path);
+    }
     return (
         <div class="content demo">
             <div class="card-hghlght demo-card">
                 <img src="/img/3.jpg " alt="3"></img>
-                <button className='btn' >Register</button>
+                <button onClick={routeChange} className='btn' >Register</button>
             </div>
             <div class="card-hghlght demo-card">
                 <img src="/img/8.jpg " alt="8"></img>
-                <button className='btn' >Register</button>
+                <button onClick={routeChange} className='btn' >Register</button>
             </div>
             <div class="card-hghlght demo-card">
                 <img src="/img/7.jpg " alt="7"></img>
-                <button className='btn' >Register</button>
+                <button onClick={routeChange} className='btn' >Register</button>
             </div>
         </div>
     )
